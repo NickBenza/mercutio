982f0ef402b807531402607e1d0b878eb94f9277
Nick Benza <macbenz@gmail.com>
2014-04-11 18:51:58 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mercutio::Application.config.secret_key_base = '0c3eb8ec4a34db555e3075e9021a6ded34d81e9314e5cc6e8e62b3d240748a72d93894663a39418ef2d0c4ff881c7c3c924d877e98cbb0b636ab33ab92f5517f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mercutio::Application.config.session_store :cookie_store, key: '_mercutio_session'
